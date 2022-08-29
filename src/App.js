e654b37d4a3a7b8be63fd73a55c41af9e2e93812
Lexi Grant <lexidgrant@gmail.com>
2022-08-29 13:47:02 -0500

    got audio to say true or false and did modal

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react'
 import {useState, useEffect} from "react"
 import axios from "axios"
 import Book from './Components/Book'
+import NewBook from './Components/NewBook'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
   const [books, setBooks] = useState([])
   const [bookToEdit, setBookToEdit] = useState(undefined)
+  
+  const [showEditModal, setShowEditModal] = useState(false)
 
   const refetchBooks = () => {
     axios.get("http://localhost:3003/books")
@@ -20,6 +24,9 @@ const App = () => {
     .then(() => {
       refetchBooks()
     })
+  }
+  const closeEditModal = () => {
+    setShowEditModal(false)
   }
   const handleEdit = (bookData) => {
     setBookToEdit(bookData)
@@ -34,15 +41,15 @@ const App = () => {
   return(
     <main>
       <h1>Book Organizer</h1>
-      <></>
+      <NewBook refetchBooks={refetchBooks} />
       <section>
-        <h2>Books to Add</h2>
+        <h2>Books Added</h2>
         <ul>
           <div className="container">
             <div className="row">
               {books.map((book) => {
                 return(
-                 <Book book={book} handleDelete={handleDelete} handleEdit={handleEdit} />
+                 <Book book={book} handleDelete={handleDelete} handleEdit={handleEdit}  />
                 )
               })}
             </div>
