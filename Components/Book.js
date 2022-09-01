d9e252e32c53bba5f4e5f7725fed507028d3d7f0
Lexi Grant <lexidgrant@gmail.com>
2022-09-01 11:46:25 -0500

    added some styling

---
+++ Components/Book.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import { Card, Container, Col, Button } from "react-bootstrap"
+import ReactModal from "react-modal"
+import { Carousel } from "react-bootstrap"
+
+
+
+const Book = ({ book, handleDelete, handleEdit }) => {
+
+  const [showSynopsisModal, setShowSynopsisModal] = useState(false)
+
+  return (
+    <>
+    
+          
+          
+          <Card className="card  m-3" style={{width:'18rem'}}>  
+            <span key={book._id}>
+              <Card.Title style={{height:"80px", marginTop:"20px"}}>{book.title}<br /></Card.Title>
+              <Card.Img variant="top" src={book.image} style={{height:"300px"}}/><br />
+              <Card.Body>
+              <Button className="one" onClick={()=> {
+                 setShowSynopsisModal(true)
+              }}>Book Information</Button>
+              <Button className="one" onClick={() => {
+                handleDelete(book)
+              }}>Delete</Button>
+              <Button className="one" onClick={() => {
+                handleEdit(book)
+              }}>Edit</Button>
+              </Card.Body>
+            </span>
+            </Card>
+            
+           
+          
+   
+      <ReactModal isOpen={showSynopsisModal}>
+        <div>
+          Genre: {book.genre}<br />
+          Synopsis: {book.synopsis}<br />
+          <details>
+            <summary>Is there an AudioBook?</summary>
+            {book.audio ? 
+            
+              <div><p>Yes</p> 
+              <button id="media"><a href={book.link}>click for audible</a></button></div>
+            : <p>No</p>}
+             
+          </details>
+          Book price: ${book.price}<br />
+          <div><button id="media"><a href={book.purchase}>click to purchase</a></button></div>
+        </div>
+        <button onClick={()=>{ setShowSynopsisModal(false)}}>close</button>
+      </ReactModal>
+     
+    </>
+  )
+
+
+}
+export default Book
---
+++ Components/NewBook.js
@@ -0,0 +1,95 @@
+import React from "react"
+import { useState } from "react"
+import axios from "axios"
+import 'bootstrap/dist/css/bootstrap.min.css';
+const API_URL = process.env.REACT_APP_API_URL
+
+const NewBook = ({ refetchBooks }) => {
+
+    const [newTitle, setNewTitle] = useState("")
+    const [newImage, setNewImage] = useState("")
+    const [newSynopsis, setNewSynopsis] = useState("")
+    const [newGenre, setNewGenre] = useState("")
+    const [newAudio, setNewAudio] = useState(false)
+    const [newLink, setNewLink] = useState("")
+    const [newPurchase, setNewPurchase] = useState("")
+    const [newPrice, setNewPrice] = useState(Number)
+
+    const handleNewTitleChange = (event) => {
+        setNewTitle(event.target.value)
+    }
+
+    const handleNewImageChange = (event) => {
+        setNewImage(event.target.value)
+    }
+
+    const handleNewSynopsisChange = (event) => {
+        setNewSynopsis(event.target.value)
+    }
+
+    const handleNewGenreChange = (event) => {
+        setNewGenre(event.target.value)
+    }
+
+    const handleNewAudioChange = (event) => {
+        setNewAudio(event.target.checked)
+    }
+
+    const handleNewLinkChange = (event) => {
+        setNewLink(event.target.value)
+    }
+
+    const handleNewPurchaseChange = (event) => {
+        setNewPurchase(event.target.value)
+    }
+
+    const handleNewPriceChange = (event) => {
+        setNewPrice(event.target.value)
+    }
+
+    const handleNewBookSubmit = (event) => {
+        event.preventDefault()
+        axios.post(
+            `${API_URL}/books`,
+            {
+                title: newTitle,
+                image: newImage,
+                synopsis: newSynopsis,
+                genre: newGenre,
+                audio: newAudio,
+                link: newLink,
+                purchase: newPurchase,
+                price: newPrice
+            }
+        ).then(() => {
+            setNewTitle("")
+            setNewImage("")
+            setNewSynopsis("")
+            setNewGenre("")
+            setNewAudio("")
+            setNewLink("")
+            setNewPurchase("")
+            setNewPrice("")
+            refetchBooks()
+        })
+    }
+
+    return (
+        <section className="top">
+            <h1 className="header">Create a Book Listing</h1>
+            <form className="form" onSubmit={handleNewBookSubmit}>
+                <input placeholder="Title" type="text" onChange={handleNewTitleChange} value={newTitle} /><br />
+                <input placeholder="Image" type="text" onChange={handleNewImageChange} value={newImage} /><br />
+                <input placeholder="Synopsis" type="text" onChange={handleNewSynopsisChange} value={newSynopsis} /><br />
+                <input placeholder="Genre" type="text" onChange={handleNewGenreChange} value={newGenre} /><br />
+                <h2 className="audiobook">Available as an AudioBook</h2> <input type="checkbox" className="checked" defaultChecked={false} onChange={handleNewAudioChange} value={newAudio} /><br />
+                <input placeholder="Link" type="text" onChange={handleNewLinkChange} value={newLink} /><br />
+                <input placeholder="Price" type="number" onChange={handleNewPriceChange} value={newPrice} /><br />
+                <input placeholder="Purchase Link" type="text" onChange={handleNewPurchaseChange} value={newPurchase} /><br />
+                <input className="btn btn-outline-secondary submitbutton" type="submit" value="submit Book" />
+            </form>
+        </section>
+                    )
+}
+
+export default NewBook
